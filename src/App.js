f56214679339768915f9e23ce4a94da460ce9677
Nuraly Tyulkubayev <tsnurali@mail.ru>
2021-10-27 05:53:13 +0600

    Create Navbar, Results, Routes, Search, Footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Routes from "./components/Routes";
+import { BrowserRouter } from 'react-router-dom'
+
 
 function App() {
+
+  const [darkTheme, setDarkTheme] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className={darkTheme ? 'dark' : ''}>
+
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
+            <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
+            <Routes />
+            <Footer />
+      </div>
     </div>
+      </BrowserRouter>
   );
 }
 
